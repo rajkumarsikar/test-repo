58845b8f2d66ca127d2604d798e172eaed14ba7b
Rajkumar Kumawat <rajkumarsikar.86@gmail.com>
2014-08-31 15:26:20 -0700

    Add Try2.py'

---
+++ try2.py
@@ -0,0 +1,30 @@
+#print("Rajkumar")
+#a="raj"+"  kumar"
+#print(a)
+#import random
+#highest=10
+#answer=random.randrange(highest)
+#guess=input("Guess a number from 0 to %d:" %highest)
+#while(int(guess)!=answer):
+#	if(int(guess)<answer):
+#		print("Answer is higher")
+#	else:
+#		print("Answer is Lower")
+#	guess=input("Guess a number from 0 to %d:" %highest)
+#input("You're a winner face!!")
+#
+#
+#class Hero:
+#	def __init__(self,name):
+#		self.name=name
+#		self.health=100
+#	def eat(self,food):
+#		if(food=='apple'):
+#			self.health -=100
+#		elif(food=='ham'):
+#			self.health +=50
+#Bob=Hero("Bob")
+#print (Bob.name)
+#print (Bob.health)
+#Bob.eat('apple')
+#print (Bob.health)
